8792108d4d61f56d454a7d802f0ee2e5ce458800
Stephen Fong <fongchinghinunsw@gmail.com>
2022-06-11 16:58:02 +0800

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,160 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: "0x0",
+  hasVoted: false,
+
+  init: function () {
+    return App.initWeb3();
+  },
+
+  // connect out client-side application to our local blockchain
+  initWeb3: function () {
+    console.log(window);
+    // web3 == window.web3
+    if (typeof web3 !== "undefined") {
+      // If a web3 instance is already provided by MetaMask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:7545"
+      );
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  },
+
+  initContract: function () {
+    $.getJSON("Election.json", function (election) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Election = TruffleContract(election);
+      // Connect provider to interact with contract
+      App.contracts.Election.setProvider(App.web3Provider);
+
+      App.listenForEvents();
+
+      return App.render();
+    });
+  },
+
+  // Listen for events emitted from the contract
+  // refresh the page once a vote is casted
+  listenForEvents: function () {
+    App.contracts.Election.deployed().then(function (instance) {
+      // Restart Chrome if you are unable to receive this event
+      // This is a known issue with Metamask
+      // https://github.com/MetaMask/metamask-extension/issues/2393
+      instance
+        .votedEvent(
+          // solidity allows us to pass a filter to the event,
+          // we won't need that so we pass an empty object
+          {},
+          // subscribe the events from the entire blockchain (from
+          // the first block to the most recent one)
+          {
+            fromBlock: 0,
+            toBlock: "latest",
+          }
+        )
+        // subscribe to the event
+        .watch(function (error, event) {
+          console.log("event triggered", event);
+          // Reload when a new vote is recorded
+          App.render();
+        });
+    });
+  },
+
+  // render out the content of the application
+  render: function () {
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
+    // Load the chosen account in metamask
+    web3.eth.getCoinbase(function (err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+    // Load contract data
+    App.contracts.Election.deployed()
+      .then(function (instance) {
+        electionInstance = instance;
+        return electionInstance.candidatesCount();
+      })
+      .then(function (candidatesCount) {
+        var candidatesResults = $("#candidatesResults");
+        candidatesResults.empty();
+
+        var candidatesSelect = $("#candidatesSelect");
+        candidatesSelect.empty();
+
+        for (var i = 1; i <= candidatesCount; i++) {
+          electionInstance.candidates(i).then(function (candidate) {
+            var id = candidate[0];
+            var name = candidate[1];
+            var voteCount = candidate[2];
+
+            // Render candidate Result
+            var candidateTemplate =
+              "<tr><th>" +
+              id +
+              "</th><td>" +
+              name +
+              "</td><td>" +
+              voteCount +
+              "</td></tr>";
+            candidatesResults.append(candidateTemplate);
+
+            // Render candidate ballot option
+            var candidateOption =
+              "<option value='" + id + "' >" + name + "</ option>";
+            candidatesSelect.append(candidateOption);
+          });
+        }
+        return electionInstance.voters(App.account);
+      })
+      .then(function (hasVoted) {
+        // Do not allow a user to vote
+        if (hasVoted) {
+          $("form").hide();
+        }
+        loader.hide();
+        content.show();
+      })
+      .catch(function (error) {
+        console.warn(error);
+      });
+  },
+
+  castVote: function () {
+    var candidateId = $("#candidatesSelect").val();
+    App.contracts.Election.deployed()
+      .then(function (instance) {
+        return instance.vote(candidateId, { from: App.account });
+      })
+      .then(function (result) {
+        // Wait for votes to update
+        $("#content").hide();
+        $("#loader").show();
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  },
+};
+
+// initialize the app whenever the window loads
+$(function () {
+  $(window).load(function () {
+    App.init();
+  });
+});
